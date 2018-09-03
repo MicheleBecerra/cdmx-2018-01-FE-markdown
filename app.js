c3a49489011710bf9bb0caff00237a7c11af8ae2
michele velazquez de la rosa becerra <michbecerra@hotmail.com>
2018-09-03 02:27:09 -0500

    Agregando la obtención de las URL`s en los documentos markdown

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
-let urlArray = []
-let patron = /'(http|https):\/\/(\w+:{0.1}\*w)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/
-let findUrl = ' '
-let dataIterada = ''
-let getUrl = ''
-
 const fs = require('fs')
-const readReadme = (callBack) => {
+const patron = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g
+const urlArray = []
+let dataIterada = ''
+const getUrl = ''
+let findUrl = ' '
+
+const readMarkDown = (callBack) => {
   fs.readFile('./README.md', 'utf8', (err, data) => {
     if (err) {
       console.log('Tienes un error')
     } else {
       callBack(data)
+      // console.log('1.1 = ' + data)
     }
   })
 }
-readReadme(callBack = (data) => {
-     // console.log(data)
-     for (let i = 0; i < data.length; i++) {
-      let iteraData = data[i]
-      dataIterada += iteraData
-      console.log(dataIterada)
+
+readMarkDown(callBack = (data) => {
+  // console.log('1.1 = ' + data)
+  for (let i = 0; i < data.length; i++) {
+    let iteraData = data[i]
+    dataIterada += iteraData
   }
+  // console.log('1.2 = ' + dataIterada)
+
+  const urlFinder = () => {
+    findUrl = dataIterada.match(patron)
+    console.log(findUrl)
+  }
+
+  urlFinder()
 })
