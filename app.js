b026392d207fd464fd5c56d8b68e05378c9128f4
michele velazquez de la rosa becerra <michbecerra@hotmail.com>
2018-09-07 01:29:47 -0500

    obteniendo un arreglo de objetos

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const fs = require('fs')
-const patron = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))/g
+const fetch = require('node-fetch')
+const patron = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-/]))/gim
+const urlName = /\[([^\]]*)\]/gim
 const urlArray = []
 let dataIterada = ''
-const getUrl = ''
 let findUrl = ' '
+let findName = ''
 
 const readMarkDown = (callBack) => {
-  fs.readFile('./README.md', 'utf8', (err, data) => {
-    if (err) {
-      console.log('Tienes un error')
+  fs.readFile('./Readme.md', 'utf8', (error, data) => {
+    if (error) {
+      console.log('Tienes un error ...')
     } else {
       callBack(data)
-      // console.log('1.1 = ' + data)
+      // console.log(data)
     }
   })
 }
@@ -22,15 +24,36 @@ readMarkDown(callBack = (data) => {
     let iteraData = data[i]
     dataIterada += iteraData
   }
-  //console.log('1.2 = ' + dataIterada)
-
-  const urlFinder = () => {
-    findUrl = dataIterada.match(patron)
-    // console.log(findUrl)
-    urlArray.push(findUrl)
-    console.log(urlArray)
-    
-  }
-
+  // console.log('1.1 = ' + dataIterada)
   urlFinder()
 })
+const urlFinder = () => {
+  findUrl = dataIterada.match(patron)
+  findName = dataIterada.match(urlName)
+  // console.log(findName)
+  if (findUrl) {
+    for (let i = 0; i < findUrl.length; i++) {
+      let request = findUrl[i]
+      fetch(request).then((response) => {
+        // console.log(response)
+        let status = response.status
+        if (status === 200) {
+          console.log('ok')
+        } else {
+          console.log('Failed')
+        }
+        const objectUrl = {
+          Name: findName[i],
+          Url: findUrl[i],
+          Status: status
+        }
+        // console.log(objectUrl)
+        urlArray.push(objectUrl)
+        console.log(urlArray)
+      })
+        .catch((error) => {
+          console.log(error.message)
+        })
+    }
+  }
+}
